9e2ec1325ea1fb25279edbda420aa8ea705b5492
Arnav Goyal <arnav.go.2003@gmail.com>
2024-02-05 22:56:07 -0500

    me when the nn is dumb

---
+++ Winter 2023-2024/AISE 3010/Assignment 1/classes.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+class neuralnet():
+
+    def relu(self, z):
+        return np.maximum(0,z)
+    
+    def relu_derivative(self, z):
+        return z > 0
+    
+    def softmax(self, z):
+        A = np.exp(z) / sum(np.exp(z))
+        return A
+
+    def init_params(self):
+        # input will be of size 784 x 1
+        # h1
+        self.w1 = np.random.randn(10, 784) # (out_size=10 x 784)(784 x 1) + (out_size x 1) = (10, 1)
+        self.b1 = np.random.randn(10, 1)
+        # h2
+        self.w2 = np.random.randn(10, 10) # (10, 10)(10 ,1) + (10,1)
+        self.b2 = np.random.randn(10, 1)
+        # h3
+        self.w3 = np.random.randn(10, 10) # (10, 10)(10 ,1) + (10,1)
+        self.b3 = np.random.randn(10, 1)
+        # output
+        self.w4 = np.random.randn(10, 10) # (10, 10)(10 ,1) + (10,1)
+        self.b4 = np.random.randn(10, 1)
+
+    def __init__(self):
+        self.init_params()
+
+    def forward(self, x):
+        fc = {}
+        fc['in'] = x
+
+        fc['z1'] = np.dot(self.w1, x) + self.b1
+        fc['a1'] = self.relu(fc['z1'])
+
+        fc['z2'] = np.dot(self.w2, fc['a1']) + self.b2
+        fc['a2'] = self.relu(fc['z2'])
+
+        fc['z3'] = np.dot(self.w3, fc['a2']) + self.b3
+        fc['a3'] = self.relu(fc['z3'])
+
+        fc['z4'] = np.dot(self.w4, fc['a3']) + self.b4
+        fc['a4'] = self.softmax(fc['z4']) # concluding softmax activation for classification
+
+        self.fc = fc
+
+        return fc['a4']
+    
+    def update_params(self, dw1, db1, dw2, db2, dw3, db3, dw4, db4, alpha):
+        self.w1 -= alpha * dw1
+        self.b1 -= alpha * db1
+        self.w2 -= alpha * dw2
+        self.b2 -= alpha * db2
+        self.w3 -= alpha * dw3
+        self.b3 -= alpha * db3
+        self.w4 -= alpha * dw4
+        self.b4 -= alpha * db4
+    
+    def backward(self, x, y, lr):
+        '''y must be one hot encoded'''
+        _ = self.forward(x) # update fc
+        fc = self.fc # obtain fc
+
+        dz4 = fc['a4'] - y
+        dw4 = np.dot(dz4, fc['a3'].T)
+        db4 = np.sum(dz4)
+
+        dz3 = np.dot(self.w4.T, dz4) * self.relu_derivative(fc['z3'])
+        dw3 = np.dot(dz3, fc['a2'].T)
+        db3 = np.sum(dz3)
+
+        dz2 = np.dot(self.w3.T, dz3) *  self.relu_derivative(fc['z2'])
+        dw2 = np.dot(dz2, fc['a1'].T)
+        db2 = np.sum(dz2)
+
+        dz1 = np.dot(self.w2.T, dz2) * self.relu_derivative(fc['z1'])
+        dw1 = np.dot(dz1, fc['in'].T)
+        db1 = np.sum(dz1)
+
+        self.update_params(dw1, db1, dw2, db2, dw3, db3, dw4, db4, alpha=lr)
+
+def one_hot_encode(Y):
+    one_hot_Y = np.zeros((10,1))
+    one_hot_Y[Y] = 1
+    return one_hot_Y
---
+++ Winter 2023-2024/AISE 3010/Assignment 1/test_classes.py
@@ -0,0 +1,63 @@
+from classes import *
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,),(0.5,))
+])
+train_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_data = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+batch_size = 1500
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+nn = neuralnet()
+
+iter_count = 0
+max_iter = 1000
+
+accs = []
+for img_batch, label_batch in train_loader:
+    succ = 0
+    # obtain single image and label
+    for i in range(img_batch.shape[0]):
+        img = img_batch[i].reshape(-1, 1) # needed to flatten this for use in a MLP
+        label = one_hot_encode(label_batch[i].numpy()) # this is now one hot encoded
+
+        res = nn.forward(img)
+
+        pred_label = np.argmax(res)
+        actual_label = np.argmax(label)
+        if (pred_label == actual_label):
+            succ += 1
+
+        nn.backward(img, label, 1e-2)
+
+    print(f'Batch {iter_count} -  Accuracy: {succ/batch_size}')
+    accs.append(succ/batch_size)
+
+    # break out of loop
+    iter_count += 1
+    if (iter_count  > max_iter):
+        break
+
+it = range(len(accs))
+print(len(it))
+print(len(accs))
+
+plt.plot(it, accs)
+plt.title('Train Acc vs Iteration')
+plt.ylabel('Train Accuracy')
+plt.xlabel('Iteration')
+plt.show()
+
+
+# nn = neuralnet()
+# label = np.array([0,0,0,0,0,0,0,1,0,0]).reshape(10,1)
+# input = np.random.randn(784, 1)
+# res = nn.forward(input)
+# nn.backward(input, label, 1e-1)
+# print(res)
